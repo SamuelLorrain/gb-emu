b90c01dc78b96906fd3670ad8ff48440d6e41458
SamuelLorrain <samuel.lorrain@gmail.com>
2022-03-27 20:25:49 +0200

    begin ppu

--- src/index.ts
+++ src/index.ts
@@ -12,81 +12,20 @@ mm.map(0, 0xffff, memory);
 mm.map(0x8000, 0xa000, vram);
 mm.map(0x0, 0x00ff, bootRom);
 
+// load testrom
 for(const [index, byte] of testRom().entries()) {
     mm.setUint8(index, byte);
 }
 
 let cpu = new Cpu(mm, new GBRegisters());
 
-// 0 to JR NZ,Addr_0007
-while(cpu.getRegister('pc') < 0x0c) {
+// "wait for screen frame"
+while(cpu.getRegister('pc') != 0x64) {
     cpu.executeNext();
 }
-cpu.executeNext(); // ld hl,$ff26
-cpu.executeNext(); // ld c,$11
-cpu.executeNext(); // ld a,$80
-cpu.executeNext(); // ld (hl-),a
-cpu.executeNext(); // ld ($ff00+c),a
-cpu.executeNext(); // inc c
-cpu.executeNext(); // ld a,$f3
-cpu.executeNext(); // ld ($ff00+c),a
-cpu.executeNext(); // ld (hl-),a
-cpu.executeNext(); // ld a,$77
-cpu.executeNext(); // ld (hl),a
-cpu.executeNext(); // ld a,$fc
-cpu.executeNext(); // ld ($ff00+47),a
-cpu.executeNext(); // ld de,$0104
-cpu.executeNext(); // ld hl,$8010
-cpu.executeNext(); // ld a,(de) // 0 because no rom is loaded
-cpu.executeNext(); // call $0095
-cpu.executeNext(); // ld c,a
-cpu.executeNext(); // ld b,$04
-while (cpu.getRegister('pc') < 0xa3) {
-    cpu.executeNext();
-}
-cpu.executeNext(); // ld (hl+),a
-cpu.executeNext(); // inc hl
-cpu.executeNext(); // ld (hl+),a
-cpu.executeNext(); // inc hl
-cpu.executeNext(); // ret
-cpu.executeNext(); // call $0096
-while (cpu.getRegister('pc') != 0x2e) {
-    cpu.executeNext();
-}
-cpu.executeNext(); // inc de
-cpu.executeNext(); // ld a,e
-cpu.executeNext(); // cp $34
-cpu.executeNext(); // jr nz, addr__0027
-while (cpu.getRegister('pc') != 0x34) {
-    cpu.executeNext();
-}
-cpu.executeNext(); // ld de,$00d8
-cpu.executeNext(); // ld b,$08
-cpu.executeNext(); // ld a,(de)
-while (cpu.getRegister('pc') != 0x40) {
-    cpu.executeNext();
-}
-cpu.executeNext(); // ld a,$19
-cpu.executeNext(); // ld ($9910),a
-cpu.executeNext(); // ld hl,$992f
-while (cpu.getRegister('pc') != 0x55) {
-    cpu.executeNext();
-}
-cpu.executeNext(); // ld h,a
-cpu.executeNext(); // ld a,$64
-cpu.executeNext(); // ld d,a
-cpu.executeNext(); // ld ($ff00 + 42), a
-cpu.executeNext(); // ld a,$91
-cpu.executeNext(); // ld ($ff00 + 40), a
-cpu.executeNext(); // inc b
-cpu.executeNext(); // ld e,$02
-cpu.executeNext(); // ld c,$0c
-cpu.executeNext(); // ld a,($FF00+44) // "wait for sreen frame"
-cpu.executeNext(); // cp, $90
-cpu.executeNext(); // jr nz, addr_0064
 cpu.debugReg();
 cpu.debugFlags();
 cpu.debugMemory(0xff42);
-cpu.debugMemory(0xff40);
-cpu.debugMemory(0xff44);
+cpu.debugMemory(0xff40); // lcdc register
+cpu.debugMemory(0xff44); // lcdc y-coordinate
 
--- src/ppu.ts
+++ src/ppu.ts
@@ -1,11 +1,70 @@
-import { MemoryMapper } from "./memorymapper";
+/**
+* Heavily inspired by : https://blog.tigris.fr/2019/09/15/writing-an-emulator-the-first-pixel/
+* and : https://www.youtube.com/watch?v=HyzD8pNlpwI (ultimate gameboy talk)
+*/
 
+type ppuState = "oamsearch"|"pixeltransfer"|"hblank"|"vblank";
 export class Ppu {
-    mmapper: MemoryMapper;
+    state: ppuState;
+    ly: number; // line y
+    ticks: number;
+    pixelDrawnsOnCurrentLine: number;
 
-    constructor(mmapper: MemoryMapper) {
-        // using all cpu mmapper instead of just
-        // vram to simplify calcs and docs
-        this.mmapper = mmapper;
+
+    // vram (8kb)
+    // oam ram (160b)
+    constructor() {
+        this.state = "oamsearch";
+        this.ly = 0;
+        this.ticks = 0;
+        this.pixelDrawnsOnCurrentLine = 0;
+    }
+
+    getState(): ppuState {
+        return this.state;
+    }
+
+    tick() {
+        switch (this.state) {
+            case "oamsearch":
+                // collect sprite data
+                // SCAN oam (object attribute memory)
+                // from 0xfe00 to 0xfe9f to mix sprit pixels in the current line later
+                // it's take 40 clocks ticks
+                if (this.ticks == 40) {
+                    this.state = "pixeltransfer";
+                }
+                break;
+            case "pixeltransfer":
+                // collect push pixels data to display
+                // fetch pixel dat ainto pixel FIFO
+                // put a pixel for the fifo on the screen
+                this.pixelDrawnsOnCurrentLine += 1;
+                if (this.pixelDrawnsOnCurrentLine == 160) {
+                    this.state = "hblank";
+                }
+                break;
+            case "hblank":
+                if (this.ticks == 456) {
+                    this.ticks = 0;
+                    this.ly += 1;
+                    if (this.ly == 144) {
+                        this.state = "vblank";
+                    } else {
+                        this.state = "oamsearch";
+                    }
+                }
+                break;
+            case "vblank":
+                if (this.ticks == 456) {
+                    this.ticks = 0;
+                    this.ly += 1;
+                    if (this.ly == 153) {
+                        this.ly += 0;
+                        this.state = "oamsearch";
+                    }
+                }
+                break;
+        }
     }
 }
