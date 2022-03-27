66520f4717f54fd9688f0498cd656cd07397848b
SamuelLorrain <samuel.lorrain@gmail.com>
2022-03-27 12:52:12 +0200

    cleaning

--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,7 @@ import { GBRegisters } from './registers';
 import { Cpu } from './cpu';
 import { createMemory } from './ram';
 import { MemoryMapper } from './memorymapper';
-import testRom from './dmg_rom';
+import testRom from '../tests_roms/dmg_rom';
 
 let bootRom = createMemory(0x100);
 let memory = createMemory(0xFFFF);
@@ -50,10 +50,43 @@ cpu.executeNext(); // ld (hl+),a
 cpu.executeNext(); // inc hl
 cpu.executeNext(); // ret
 cpu.executeNext(); // call $0096
+while (cpu.getRegister('pc') != 0x2e) {
+    cpu.executeNext();
+}
+cpu.executeNext(); // inc de
+cpu.executeNext(); // ld a,e
+cpu.executeNext(); // cp $34
+cpu.executeNext(); // jr nz, addr__0027
+while (cpu.getRegister('pc') != 0x34) {
+    cpu.executeNext();
+}
+cpu.executeNext(); // ld de,$00d8
+cpu.executeNext(); // ld b,$08
+cpu.executeNext(); // ld a,(de)
+while (cpu.getRegister('pc') != 0x40) {
+    cpu.executeNext();
+}
+cpu.executeNext(); // ld a,$19
+cpu.executeNext(); // ld ($9910),a
+cpu.executeNext(); // ld hl,$992f
+while (cpu.getRegister('pc') != 0x55) {
+    cpu.executeNext();
+}
+cpu.executeNext(); // ld h,a
+cpu.executeNext(); // ld a,$64
+cpu.executeNext(); // ld d,a
+cpu.executeNext(); // ld ($ff00 + 42), a
+cpu.executeNext(); // ld a,$91
+cpu.executeNext(); // ld ($ff00 + 40), a
+cpu.executeNext(); // inc b
+cpu.executeNext(); // ld e,$02
+cpu.executeNext(); // ld c,$0c
+cpu.executeNext(); // ld a,($FF00+44) // "wait for sreen frame"
+cpu.executeNext(); // cp, $90
+cpu.executeNext(); // jr nz, addr_0064
 cpu.debugReg();
 cpu.debugFlags();
-cpu.debugMemory(0xfffa, 5);
-// cpu.debugMemory(0xff11, 5);
-// cpu.debugMemory(0xff26, 5);
-// cpu.debugMemory(0xff47);
+cpu.debugMemory(0xff42);
+cpu.debugMemory(0xff40);
+cpu.debugMemory(0xff44);
 
